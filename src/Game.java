b9961df91939d965f51df943e5e9192b8a723a74
Sophia Rodriguez-Bell <sophiarodriguezbell@students.berkeley.net>
2019-02-12 14:25:55 -0800

    keep going, check error

--- src/Game.java
+++ src/Game.java
@@ -2,13 +2,32 @@ import java.util.Random;
 
 public class Game {
 
-    private String[] selection = {"apple", "pear", "banana"};
-    private int placement = new Random().nextInt(selection.length);
-    private String word = selection[placement];
-    int guesses;
+    String[] guesses;
+    String word;
+    int guessNumber;
 
     public Game() {
-        this.guesses = 0;
+        String[] selection = {"apple", "pear", "banana"};
+        int placement = new Random().nextInt(selection.length);
+        this.word = selection[placement];
+        this.guesses = new String[word.length()];
+        this.guessNumber = 0;
+    }
+
+    public String guessLetter(String letter) {
+        this.guesses[this.guessNumber] = letter;
+        String check = this.word + " ";
+        String builder = "";
+        for (int a = 0; a < this.word.length(); a++){
+            for (int b = 0; b < this.guesses.length; b++){
+                if(check.substring(a, a+1).equals(b)){
+                    builder += b;
+                } else {
+                    builder += "_";
+                }
+            }
+        }
+        return builder;
     }
 
     // Scanner sc = new Scanner(System.in);
--- src/Runner.java
+++ src/Runner.java
@@ -2,7 +2,13 @@ import java.util.Scanner;
 
 public class Runner {
 
-    Scanner sc = new Scanner(System.in);
-    System.
+    public static void main(){
+        Game round = new Game();
+        Scanner sc = new Scanner(System.in);
+        System.out.println(System.in);
+        System.out.println("Enter your guess.");
+        String letter = sc.nextLine();
+        System.out.println(round.guessLetter(letter));
+    }
 
 }
