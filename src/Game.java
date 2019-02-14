0bff26f0d486ce86d08f8dd798ce02dad7ee1682
Sophia Rodriguez-Bell <sophiarodriguezbell@students.berkeley.net>
2019-02-14 14:23:49 -0800

    figure out how to add a space and not have a wrong answer mess everything up... have fun! yay for DW and G1! :D

--- src/Game.java
+++ src/Game.java
@@ -4,7 +4,6 @@ public class Game {
 
     String[] guesses;
     String word;
-    String progress;
     int guessNumber;
 
     public Game() {
@@ -12,23 +11,41 @@ public class Game {
         int placement = new Random().nextInt(selection.length);
         this.word = selection[placement];
         this.guesses = new String[word.length()];
-        this.guessNumber = 0;
+        this.guessNumber = -1;
     }
 
     public String guessLetter(String letter) {
+        this.guessNumber++;
         this.guesses[this.guessNumber] = letter;
         String check = this.word + " ";
         String builder = "";
         for (int a = 0; a < this.word.length(); a++){
             for (int b = 0; b < this.guesses.length; b++){
-                if(check.substring(a, a+1).equals(b)){
-                    builder += b;
-                } else {
-                    builder += "_";
+                String test1 = check.substring(a, a+1);
+                if(check.substring(a, a+1).equals(this.guesses[b])){
+                    builder += this.guesses[b];
                 }
+            }
+            int test = builder.length();
+            if (builder.length() - 1 != a){
+                builder += "_ ";
             }
         }
         return builder;
+    }
+
+    public String initialBlank(){
+        String builder = "";
+        for (int a = 0; a < this.word.length(); a++){
+            builder += "_ ";
+        }
+        return builder;
+    }
+
+    public int getGuessNumber() {
+
+        return guessNumber;
+
     }
 
     // Scanner sc = new Scanner(System.in);
--- src/Runner.java
+++ src/Runner.java
@@ -4,16 +4,18 @@ public class Runner {
 
     public static void main(String[] args){
         Game round = new Game();
-        Scanner sc = new Scanner(System.in);
-        System.out.println(System.in);
-        System.out.println("Enter your guess.");
-        if(sc.hasNextLine()) {
-            String input = sc.nextLine();
-            round.guessLetter(input);
-        } else {
-            System.out.println("This is not a letter!");
+        System.out.println(round.initialBlank());
+        while (round.getGuessNumber() <= 8){
+            Scanner sc = new Scanner(System.in);
+            System.out.println(System.in);
+            System.out.println("Enter your guess.");
+            if (sc.hasNextLine()) {
+                String input = sc.nextLine();
+                System.out.println(round.guessLetter(input));
+            } else {
+                System.out.println("This is not a letter!");
+            }
         }
-
     }
 
 }
